ab2a133e916ac56d03bec55ad91311647403582e
Dipendu Ghosh <dipendughosh@gmail.com>
2025-08-09 00:05:45 +0530

    macropad_v1_24_32u4 keep display helpers unique with 2040.

--- keyboards/diydipendu/macropad24_32u4/rules.mk
+++ keyboards/diydipendu/macropad24_32u4/rules.mk
@@ -2,15 +2,15 @@
 OLED_ENABLE = yes
 
 # Set the OLED driver (SSD1306 is the most common)
-OLED_DRIVER = ssd1306
+# OLED_DRIVER = ssd1306
 
 # Enable WPM to display your typing speed
 WPM_ENABLE = yes
 
 # Define the I2C pins for the OLED. These are the correct pins for a Pro Micro.
-OLED_I2C_SDA_PIN = D1
-OLED_I2C_SCL_PIN = D0
+# OLED_I2C_SDA_PIN = D1
+# OLED_I2C_SCL_PIN = D0
 
 # Optional: Set the display size and a timeout
-OLED_DISPLAY_128X64 = yes
-OLED_TIMEOUT = 10000 # 10 seconds
+# OLED_DISPLAY_128X64 = yes
+# OLED_TIMEOUT = 10000 # 10 seconds
