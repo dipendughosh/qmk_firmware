665283f539563809b0fdf19f85f57d84fa736578
Dipendu Ghosh <dipendughosh@gmail.com>
2025-08-08 23:34:41 +0530

    macro_v1_24_2040 display support

---
+++ keyboards/diydipendu/macropad24_2040/config.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#define I2C1_SDA_PIN GP0
+#define I2C1_SCL_PIN GP1
+
+#ifdef OLED_ENABLE
+#   define OLED_DISPLAY_128X64
+#   define I2C_DRIVER I2CD0
+#   define OLED_TIMEOUT 60000
+#   define OLED_BRIGHTNESS 128
+#   define OLED_FONT_H "keyboards/1upkeyboards/pi40/lib/glcdfont.c"
+#endif
